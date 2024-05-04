b074e4d5aadbd9125aa8cbd0fd697ca5587569bb
MwCplace <134137626+MwCplace@users.noreply.github.com>
2024-05-04 19:18:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var elements = document.getElementsByClassName("column");
+const bigLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const smallLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const elements = document.getElementsByClassName("column");
 
 function one() {
   for (i = 0; i < elements.length; i++) {
@@ -18,4 +20,22 @@ function four() {
   for (i = 0; i < elements.length; i++) {
       elements[i].style.flex = "12%";
   }
+}
+
+const container = document.getElementsByClassName('card-cnt');
+
+for (let i = 0; i < bigLetters.length; i++) {
+  const el1 = document.createElement('div');
+  el1.classList.add("card");
+  el1.classList.add("column");
+  container[0].appendChild(el1);
+  // document.createElement('h2');
+}
+
+const titles = document.querySelectorAll('h2');
+const subtitles = document.querySelectorAll('p');
+
+for (let i = 0; i < bigLetters.length; i++) {
+  console.log(titles[i]);
+  titles[i].innerHTML = bigLetters[i] + " " + smallLetters[i];
 }
